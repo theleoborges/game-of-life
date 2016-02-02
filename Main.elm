5277b4e3011c5fc289427da53344fa87248ff8a1
Leonardo Borges <leonardoborges.rj@gmail.com>
2016-02-02 11:47:06 +1100

    Sweet. Found the bug :)

--- Main.elm
+++ Main.elm
@@ -1,14 +1,10 @@
 module Main where
 
-import Array exposing (Array, repeat, initialize)
 import Color exposing (..)
 import Graphics.Collage exposing (..)
 import Graphics.Element exposing (..)
-import Random
-import Debug exposing (log)
 import Time exposing (Time)
 import Dict exposing (Dict)
-import Maybe exposing (withDefault)
 
 -- MODEL
 
@@ -33,13 +29,29 @@ cells' rows columns =
             [0..columns]
               |> List.map
                  (\x ->
-                    let alive = List.member (x,y) glider--x % 3 == 0 && x % 5 == 0
-                    in ((x, -y) , (Cell (x * 10) (-y * 10) 10 10 alive))))
+--                    let alive = x % 9 == 0 || x % 3 /= 0
+                    let alive = List.member (x,y) grower
 
-glider = [(1,0), (2,1), (0,2), (1,2), (2,2)]
+                        x'     = x * 10
+                        y'     = -y * 10
+                    in ((x, y) , (Cell x' y' 10 10 alive))))
 
+glider = [(11,0), (12,1), (10,2), (11,2), (12,2)]
+gliderGun = [(3,7),(4,7),(3,8),(4,8),(13,7),(13,8),(13,9),(14,6),(14,10),(15,5),(16,5),(15,11),(16,11),(17,8),(18,6),(18,10),(19,7),(19,8),(19,9),(20,8),(23,5),(24,5),(23,6),(24,6),(23,7), (24,7),(25,4),(25,8), (27,3), (27,4),(27,8),(27,9),(37,5),(37,6),(38,5),(38,6)]
+grower = [(20,30),(22,30),(22,29),(24,28),(24,27),(24,26),(26,27),(26,26),(26,25),(27,26)]
 -- UPDATE
-numAliveNeighbours dict (x,y) cell =
+
+update : Time -> Model -> Model
+--update _ model = Debug.watch "model" model
+update _ model =
+  let folder coord cell dict =
+        Dict.insert coord (handleCell model.generation coord cell) dict
+      generation' = Dict.foldl folder model.generation model.generation
+  in
+  { model | generation = generation' }
+
+
+numAliveNeighbours dict (x,y) =
   let neighbours = [(x - 1, y - 1),
                     (x - 1, y),
                     (x - 1, y + 1),
@@ -53,16 +65,16 @@ numAliveNeighbours dict (x,y) cell =
           (Just cell) -> cell.alive
           Nothing     -> False
   in List.map livingStatus neighbours
-      |> List.foldr (\status acc -> if status then acc + 1 else acc) 0
+      |> List.foldl (\status acc -> if status then acc + 1 else acc) 0
 
 handleLivingCell dict coord cell =
-  let n = numAliveNeighbours dict coord cell
+  let n = numAliveNeighbours dict coord
   in if n < 2 || n > 3
      then {cell | alive = False}
      else cell
 
 handleDeadCell dict coord cell =
-  let n = numAliveNeighbours dict coord cell
+  let n = numAliveNeighbours dict coord
   in if n == 3
      then {cell | alive = True}
      else cell
@@ -73,19 +85,13 @@ handleCell dict coord cell =
     True  -> handleLivingCell dict coord cell
     False -> handleDeadCell  dict coord cell
 
-update : Time -> Model -> Model
-update _ model =
-  let folder coord dict =
-        case Dict.get coord dict of
-          (Just cell') -> Dict.insert coord (handleCell dict coord cell') dict
-          Nothing      -> Dict.empty
-      generation' = List.foldl folder model.generation (Dict.keys model.generation)
-  in
-  { model | generation = generation' }
-
-
 
 -- VIEW
+
+view : Model -> Element
+view {generation} =
+  List.map cell (Dict.values generation)
+    |> collage 800 800
 
 cell : Cell -> Form
 cell {x, y, height, width, alive} =
@@ -94,18 +100,8 @@ cell {x, y, height, width, alive} =
     |> move (toFloat x, toFloat y)
 
 
-view : Model -> Element
-view {generation} =
-  List.map cell (Dict.values generation)
-    |> collage 800 800
-
-
-
--- main : Element
--- main =
---   List.map view (cells 10 10)
---     |> collage 600 600
+-- MAIN
 
 main : Signal Element
 main =
-  Signal.map view (Signal.foldp update (init 30 30) (Time.fps 1))
+  Signal.map view (Signal.foldp update (init 40 40) (Time.fps 10))
